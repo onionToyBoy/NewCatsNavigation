85bf1e581fff4b7391b7fc2df5a90bdf99043fef
onionToyBoy <grazdaninzemlidobri@gmail.com>
2021-04-28 13:28:20 +0300

    AddNewProject

---
+++ App.js
@@ -0,0 +1,12 @@
+import 'react-native-gesture-handler'
+import React from 'react'
+import MyStack from './MyNavigator'
+
+const App = ({navigation}) => {
+
+  return (
+       <MyStack/>
+  )
+}
+
+export default App
---
+++ MyNavigator.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import FirstScreen from './FirstScreen'
+import SecondScreen from './SecondScreen'
+
+const Stack = createStackNavigator()
+
+function MyStack() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen 
+                    name="Main" 
+                    component={FirstScreen} 
+                    options={{headerTitleAlign:'center',
+                        headerStyle : { 
+                        backgroundColor:'#7C42DCD0'}}}
+                />
+                <Stack.Screen 
+                    name="Info" 
+                    component={SecondScreen} 
+                    options={{headerTitleAlign:'center',
+                         headerStyle : { 
+                         backgroundColor:'#7C42DCD0'}
+                    }}
+                   
+                    
+                />
+            </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+
+export default MyStack
